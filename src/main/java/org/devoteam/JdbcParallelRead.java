37b70d4f5d0b2b796703ae89d4b23f19c45d5958
chiheb.dkhil <chiheb.dkhil@localhost>
2020-01-15 16:02:06 -0500

    rr

--- src/main/java/org/devoteam/JdbcParallelRead.java
+++ src/main/java/org/devoteam/JdbcParallelRead.java
@@ -21,10 +21,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation.Required;
 import org.apache.beam.sdk.transforms.*;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
@@ -45,114 +50,128 @@ import java.sql.ResultSet;
  *   //    mvn compile exec:java -D exec.mainClass=org.devoteam.JdbcParallelRead -D exec.args="--runner=DataflowRunner --project=<YOUR_PROJECT> --region=<YOUR_REGION> --zone=<YOUR_ZONE> --gcpTempLocation=gs://<YOUR_BUCKET>/tmp/"
  */
 public class JdbcParallelRead {
-  private static final Logger LOG = LoggerFactory.getLogger(JdbcParallelRead.class);
-  @SuppressWarnings("serial")
-public static void main(String[] args) throws PropertyVetoException {
-      ComboPooledDataSource dataSource = new ComboPooledDataSource();
-      dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-      dataSource.setJdbcUrl("jdbc:mysql://google/employees?cloudSqlInstance=celtic-list-244219:us-central1:cdf01" +
-              "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false" +
-              "&user=cdf01&password=cdf01");
+	private static final Logger LOG = LoggerFactory.getLogger(JdbcParallelRead.class);
+	@SuppressWarnings("serial")
+	public static void main(String[] args) throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+		dataSource.setJdbcUrl("jdbc:mysql://google/employees?cloudSqlInstance=celtic-list-244219:us-central1:cdf01" +
+				"&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false" +
+				"&user=cdf01&password=cdf01");
 
 
-      dataSource.setMaxPoolSize(10);
-      dataSource.setInitialPoolSize(6);
-      JdbcIO.DataSourceConfiguration config
-              = JdbcIO.DataSourceConfiguration.create(dataSource);
+		dataSource.setMaxPoolSize(10);
+		dataSource.setInitialPoolSize(6);
+		JdbcIO.DataSourceConfiguration config
+		= JdbcIO.DataSourceConfiguration.create(dataSource);
 
-    Pipeline p = Pipeline.create(
-        PipelineOptionsFactory.fromArgs(args).withValidation().create());
-    String tableName = "employees";
-    int fetchSize = 1000;
-    
-    System.out.println(" --- toto");
-    
-//    Create range index chunks Pcollection
-    PCollection<KV<String,Iterable<Integer>>> ranges =
-            p.apply(String.format("Read from Cloud SQL MySQL: %s",tableName), JdbcIO.<String>read()
-            .withDataSourceConfiguration(config)
-            .withQuery(String.format("SELECT MAX(`emp_no`) from %s", tableName))
-            .withRowMapper(new JdbcIO.RowMapper<String>() {
-                public String mapRow(ResultSet resultSet) throws Exception {
-                    return resultSet.getString(1);
-                }
-            })
-            .withOutputParallelization(false)
-            .withCoder(StringUtf8Coder.of()))
-            .apply("Distribute", ParDo.of(new DoFn<String, KV<String, Integer>>() {
-                @ProcessElement
-                public void processElement(ProcessContext c) {
-                    int readChunk = fetchSize;
-                    int count = Integer.parseInt((String) c.element());
-                    int ranges = (int) (count / readChunk);
-                    for (int i = 0; i < ranges; i++) {
-                        int indexFrom = i * readChunk;
-                        int indexTo = (i + 1) * readChunk;
-                        String range = String.format("%s,%s",indexFrom, indexTo);
-                        KV<String,Integer> kvRange = KV.of(range, 1);
-                        c.output(kvRange);
-                    }
-                    if (count > ranges * readChunk) {
-                        int indexFrom = ranges * readChunk;
-                        int indexTo = ranges * readChunk + count % readChunk;
-                        String range = String.format("%s,%s",indexFrom, indexTo);
-                        KV<String,Integer> kvRange = KV.of(range, 1);
-                        c.output(kvRange);
-                    }
-                }
-            }))
-            .apply("Break Fusion", GroupByKey.create())
-    ;
+		JPOptions options =
+		        PipelineOptionsFactory.fromArgs(args).withValidation().as(JPOptions.class);
+		
+		Pipeline p = Pipeline.create(options);
+		
+		String tableName = "employees";
+		int fetchSize = 1000;
+
+		LOG.info(" ********* STARTED **************");
+
+		//    Create range index chunks Pcollection
+		PCollection<KV<String,Iterable<Integer>>> ranges =
+				p.apply(String.format("Read from Cloud SQL MySQL: %s",tableName), JdbcIO.<String>read()
+						.withDataSourceConfiguration(config)
+						.withQuery(String.format("SELECT MAX(`emp_no`) from %s", tableName))
+						.withRowMapper(new JdbcIO.RowMapper<String>() {
+							public String mapRow(ResultSet resultSet) throws Exception {
+								return resultSet.getString(1);
+							}
+						})
+						.withOutputParallelization(false)
+						.withCoder(StringUtf8Coder.of()))
+				.apply("Distribute", ParDo.of(new DoFn<String, KV<String, Integer>>() {
+					@ProcessElement
+					public void processElement(ProcessContext c) {
+						int readChunk = fetchSize;
+						int count = Integer.parseInt((String) c.element());
+						int ranges = (int) (count / readChunk);
+						for (int i = 0; i < ranges; i++) {
+							int indexFrom = i * readChunk;
+							int indexTo = (i + 1) * readChunk;
+							String range = String.format("%s,%s",indexFrom, indexTo);
+							KV<String,Integer> kvRange = KV.of(range, 1);
+							c.output(kvRange);
+						}
+						if (count > ranges * readChunk) {
+							int indexFrom = ranges * readChunk;
+							int indexTo = ranges * readChunk + count % readChunk;
+							String range = String.format("%s,%s",indexFrom, indexTo);
+							KV<String,Integer> kvRange = KV.of(range, 1);
+							c.output(kvRange);
+						}
+					}
+				}))
+				.apply("Break Fusion", GroupByKey.create())
+				;
 
 
-      ranges.apply(String.format("Read ALL %s", tableName), JdbcIO.<KV<String,Iterable<Integer>>,String>readAll()
-              .withDataSourceConfiguration(config)
-              .withFetchSize(fetchSize)
-              .withCoder(StringUtf8Coder.of())
-              .withParameterSetter(new JdbcIO.PreparedStatementSetter<KV<String,Iterable<Integer>>>() {
-                  @Override
+		ranges.apply(String.format("Read ALL %s", tableName), JdbcIO.<KV<String,Iterable<Integer>>,String>readAll()
+				.withDataSourceConfiguration(config)
+				.withFetchSize(fetchSize)
+				.withCoder(StringUtf8Coder.of())
+				.withParameterSetter(new JdbcIO.PreparedStatementSetter<KV<String,Iterable<Integer>>>() {
+					@Override
 
-                  public void setParameters(KV<String,Iterable<Integer>> element,
-                                            PreparedStatement preparedStatement) throws Exception {
+					public void setParameters(KV<String,Iterable<Integer>> element,
+							PreparedStatement preparedStatement) throws Exception {
 
-                      String[] range = element.getKey().split(",");
-                      preparedStatement.setInt(1, Integer.parseInt(range[0]));
-                      preparedStatement.setInt(2, Integer.parseInt(range[1]));
-                  }
-              })
-                      .withOutputParallelization(false)
-              .withQuery(String.format("select * from employees.%s where emp_no >= ? and emp_no < ?",tableName))
-                      .withRowMapper((JdbcIO.RowMapper<String>) resultSet -> {
-                          ObjectMapper mapper = new ObjectMapper();
-                          ArrayNode arrayNode = mapper.createArrayNode();
-                          for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
-                              String columnTypeIntKey ="";
-                              try {
-                                  ObjectNode objectNode = mapper.createObjectNode();
-                                  objectNode.put("column_name",
-                                          resultSet.getMetaData().getColumnName(i));
+						String[] range = element.getKey().split(",");
+						preparedStatement.setInt(1, Integer.parseInt(range[0]));
+						preparedStatement.setInt(2, Integer.parseInt(range[1]));
+					}
+				})
+				.withOutputParallelization(false)
+				.withQuery(String.format("select * from employees.%s where emp_no >= ? and emp_no < ?",tableName))
+				.withRowMapper((JdbcIO.RowMapper<String>) resultSet -> {
+					ObjectMapper mapper = new ObjectMapper();
+					ArrayNode arrayNode = mapper.createArrayNode();
+					for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+						String columnTypeIntKey ="";
+						try {
+							ObjectNode objectNode = mapper.createObjectNode();
+							objectNode.put("column_name",
+									resultSet.getMetaData().getColumnName(i));
 
-                                  objectNode.put("value",
-                                          resultSet.getString(i));
-                                  arrayNode.add(objectNode);
-                              } catch (Exception e) {
-                                  LOG.error("problem columnTypeIntKey: " +  columnTypeIntKey);
-                                  throw e;
-                              }
-                          }
-                          return mapper.writeValueAsString(arrayNode);
-                      })
-              )
-              .apply(MapElements.via(
-                      new SimpleFunction<String, Integer>() {
-                          @Override
-                          public Integer apply(String line) {
-                              return line.length();
-                          }
-                      }))
-      ;
+							objectNode.put("value",
+									resultSet.getString(i));
+							arrayNode.add(objectNode);
+						} catch (Exception e) {
+							LOG.error("problem columnTypeIntKey: " +  columnTypeIntKey);
+							throw e;
+						}
+					}
+					return mapper.writeValueAsString(arrayNode);
+				})
+				)
+		.apply(MapElements.via(
+				new SimpleFunction<String, String>() {
+					@Override
+					public String apply(String line) {
+						return "Line: " + line.length();
+					}
+				}))
+		 .apply("WriteCounts", TextIO.write().to(options.getOutput()));
+		;
 
-    p.run();
-  }
+		p.run();
+	}
+	
+	  public interface JPOptions extends PipelineOptions {
+
+		    /** Set this required option to specify where to write the output. */
+		    @Description("Path of the file to write to")
+		    @Required
+		    String getOutput();
+
+		    void setOutput(String value);
+		  }
 }
 
