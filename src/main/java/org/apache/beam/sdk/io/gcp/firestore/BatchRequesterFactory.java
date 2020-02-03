1f5f6ed0cb53d998b78acbac945b2b9fb688ec59
Chiheb Dkhil <chiheb.dkhil@S05259-MBPR.local>
2020-02-03 15:44:07 -0500

    test

--- src/main/java/org/apache/beam/sdk/io/gcp/firestore/BatchRequesterFactory.java
+++
@@ -1,9 +0,0 @@
-package org.apache.beam.sdk.io.gcp.firestore;
-
-import com.google.cloud.firestore.Firestore;
-
-import java.io.Serializable;
-
-public interface BatchRequesterFactory<T> extends Serializable {
-    FirestoreBatchRequester<T> create(Firestore firestore);
-}
